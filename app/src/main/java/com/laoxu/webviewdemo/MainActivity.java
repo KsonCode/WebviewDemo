4acf4497b7b4e2ccdf5239179f4b07877c8a43e0
YangShuai1612b <1152722231@qq.com>
2019-12-17 15:01:28 +0800

    Initial commit

---
+++ app/src/main/java/com/laoxu/webviewdemo/MainActivity.java
@@ -0,0 +1,88 @@
+package com.laoxu.webviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+        initSettings();//webview的设置
+
+    }
+
+    /**
+     * webview的设置
+     */
+    private void initSettings() {
+        //支持js，变成动态，可以传递数据
+        webView.getSettings().setJavaScriptEnabled(true);
+
+
+
+    }
+
+    private void initView() {
+
+        webView =findViewById(R.id.webview);
+
+        //支持弹窗，处理webview加载过程中的监听,可以在应用内加载
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+    }
+
+    public void loadUrl(View view) {
+
+        //加载一个远程网页
+        webView.loadUrl("https://www.baidu.com/");
+    }
+
+    public void loadLocal(View view) {
+
+        // 先载入JS代码
+        // 格式规定为:file:///android_asset/文件名.html
+        webView.loadUrl("file:///android_asset/hellojs.html");
+    }
+
+    /**
+     * 加载局部代码
+     * @param view
+     */
+    public void loadData(View view) {
+
+        String s = "<p>我是一个段落</p>\n" +
+                "<div id=\"content\"></div>\n" +
+                "<button onclick=\"click()\" >点击</button>";
+
+        webView.loadData(s,"text/html; charset=UTF-8",null);
+    }
+
+    /**
+     * android 传递数据给js（网站的js代码）
+     * @param view
+     */
+    public void androidtojs(View view) {
+
+        int age = 50;
+
+        //无参数
+        webView.loadUrl("javascript:name()");
+        //有参数，年龄
+        webView.loadUrl("javascript:age('"+age+"')");
+    }
+}
